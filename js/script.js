5f07b8c9dff3541741bc3e8be8edfbb63b71a9dd
Hasibur Rahman <biplobjr29@gmail.com>
2023-12-31 08:50:41 +0600

    commit-1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+new WOW().init();
+
+$('.customer-review').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 1500,
+    arrows:false,
+    dots:true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
